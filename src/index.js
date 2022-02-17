72d667db11a41bc68f336f65f4cc9e07d993296a
Roman-Nechaev <nrchaevroman@gmail.com>
2022-02-17 22:34:57 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import countriesTpl from './templates/countries_template.hbs';
+import countriesInfoTpl from './templates/country-card.hbs';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
+const searchInput = document.querySelector('#search-box');
+const countriesList = document.querySelector('.country-list');
+const countriesInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+searchInput.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+searchInput.focus();
+
+let name = '';
+
+function onInputSearch(event) {
+  name = event.target.value.trim();
+
+  clearInput();
+  setCountries(name);
+  function setCountries(name) {
+    fetchCountries(name)
+      .then(data => {
+        const amount = data.length;
+
+        if (amount > 10) {
+          Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+          return;
+        }
+        if (amount >= 2 && amount <= 10) {
+          renderCountriesMarkup(data);
+        } else {
+          renderCountriesInfoMarkup(data);
+        }
+      })
+      .catch(onFetchError);
+  }
+}
+
+function onFetchError(error) {
+  if (name !== '') {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+  }
+}
+
+function renderCountriesMarkup(data) {
+  countriesList.insertAdjacentHTML('beforeend', countriesTpl(data));
+}
+
+function renderCountriesInfoMarkup(data) {
+  countriesInfo.insertAdjacentHTML('beforeend', countriesInfoTpl(data));
+}
+
+function clearInput() {
+  countriesList.innerHTML = '';
+  countriesInfo.innerHTML = '';
+}
